39e85edb856c836c1b0299638738ca548e9fc38c
Rayan Avelino <rayanodb@gmail.com>
2022-04-21 17:46:50 -0300

    fix: title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Contats</title>
+        <title>Contacts</title>
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
